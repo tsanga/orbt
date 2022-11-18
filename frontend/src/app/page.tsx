37c63ea206886143d54175108c62acf861a6b073
Alexander Maxwell Adewole <alex@bizar.re>
2022-11-18 02:12:04 -0800

    Delete old frontend dir

--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>orbt</h1>
-    </div>
-  );
-}
--- frontend/src/theme/light-theme.css.ts
+++
@@ -1,6 +0,0 @@
-import { createTheme } from "@vanilla-extract/css";
-import { vars } from "./contract.css";
-
-// dark theme is prio, will go ahead and update this theme once
-// all the vars are more finalized
-export const lightThemeClass = "";
