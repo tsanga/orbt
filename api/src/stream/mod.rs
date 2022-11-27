1f8cc656647332af16a0ea670a5a1a3f6d27bdb5
Jonah Seguin <me@jonahseguin.com>
2022-11-27 02:03:11 -0700

    API MVP (#13)
    * api: tests for room model, color queries, unique chat msg ids
    
    * api: remove schema file, add to gitignore
    
    * api: convert roommember to complex object to allow for fetching user obj subfields
    
    * api: fix unit test for room_leave
    
    * api: new type type prototyping
    
    * api: new id & model type impl, new datastore impl
    
    * api: new stream system to allow handle connection disconnecting
    
    * api: impl new stream system w/ schema subscriptions to handle disconnects
    
    * api: remove skidding
    
    * api: update schema query/mutation naming scheme

--- api/src/stream/mod.rs
+++ api/src/stream/mod.rs
@@ -1,68 +1,223 @@
 use std::{
     any::{Any, TypeId},
     collections::HashMap,
-    marker::PhantomData,
     pin::Pin,
-    sync::Mutex,
+    sync::{Arc, Mutex},
     task::{Context, Poll},
 };
 
 use futures::channel::mpsc::{self, UnboundedReceiver, UnboundedSender};
 use futures_util::{Stream, StreamExt};
-use once_cell::sync::Lazy;
 use slab::Slab;
 
-static SUBSCRIBERS: Lazy<Mutex<HashMap<TypeId, Box<dyn Any + Send>>>> = Lazy::new(Default::default);
+use crate::{
+    model::{room::Room, user::User},
+    store::DataStore,
+    types::id::Id,
+};
 
-struct Senders<T>(Slab<UnboundedSender<T>>);
+pub struct StreamSenders<T: Send + Sync + Clone + 'static>(Slab<UnboundedSender<T>>);
 
-struct BrokerStream<T: Sync + Send + Clone + 'static>(usize, UnboundedReceiver<T>);
-
-fn with_senders<T, F, R>(f: F) -> R
-where
-    T: Sync + Send + Clone + 'static,
-    F: FnOnce(&mut Senders<T>) -> R,
-{
-    let mut map = SUBSCRIBERS.lock().unwrap();
-    let senders = map
-        .entry(TypeId::of::<Senders<T>>())
-        .or_insert_with(|| Box::new(Senders::<T>(Default::default())));
-    f(senders.downcast_mut::<Senders<T>>().unwrap())
+#[derive(Clone)]
+pub struct StreamController {
+    subscribers: Arc<Mutex<HashMap<TypeId, Box<dyn Any + Send>>>>,
 }
 
-impl<T: Sync + Send + Clone + 'static> Drop for BrokerStream<T> {
-    fn drop(&mut self) {
-        with_senders::<T, _, _>(|senders| senders.0.remove(self.0));
+impl StreamController {
+    pub fn new() -> Self {
+        Self {
+            subscribers: Arc::new(Mutex::new(HashMap::new())),
+        }
     }
-}
 
-impl<T: Sync + Send + Clone + 'static> Stream for BrokerStream<T> {
-    type Item = T;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.1.poll_next_unpin(cx)
-    }
-}
-
-/// A simple broker based on memory
-pub struct OrbtStreamBroker<T>(PhantomData<T>);
-
-impl<T: Sync + Send + Clone + 'static> OrbtStreamBroker<T> {
-    /// Publish a message that all subscription streams can receive.
-    pub fn publish(msg: T) {
-        with_senders::<T, _, _>(|senders| {
+    pub fn publish<T: Send + Sync + Clone + 'static>(&self, msg: T) {
+        with_senders::<T, _, _>(&self, |senders| {
             for (_, sender) in senders.0.iter_mut() {
                 sender.start_send(msg.clone()).ok();
             }
         });
     }
 
-    /// Subscribe to the message of the specified type and returns a `Stream`.
-    pub fn subscribe() -> impl Stream<Item = T> {
-        with_senders::<T, _, _>(|senders| {
-            let (tx, rx) = mpsc::unbounded();
-            let id = senders.0.insert(tx);
-            BrokerStream(id, rx)
+    pub fn subscribe<'a, T, F>(
+        &'a self,
+        user: &Id<User>,
+        room: &Id<Room>,
+        user_store: DataStore<User>,
+        room_store: DataStore<Room>,
+        on_drop: F,
+    ) -> impl Stream<Item = T> + 'a
+    where
+        T: Send + Sync + Clone + 'static,
+        F: FnOnce(&UserStreamData) + 'a,
+    {
+        with_senders::<T, _, _>(&self, |senders| {
+            let (sender, receiver) = mpsc::unbounded();
+            let id = senders.0.insert(sender);
+            UserStreamSubscriber::<T, F>::new(
+                id, receiver, user, room, user_store, room_store, on_drop, &self,
+            )
         })
+    }
+}
+
+fn with_senders<T, F, R>(ctl: &StreamController, func: F) -> R
+where
+    T: Send + Sync + Clone + 'static,
+    F: FnOnce(&mut StreamSenders<T>) -> R,
+{
+    let mut map = ctl.subscribers.lock().unwrap();
+    let senders = map
+        .entry(TypeId::of::<StreamSenders<T>>())
+        .or_insert_with(|| Box::new(StreamSenders::<T>(Default::default())));
+    func(senders.downcast_mut::<StreamSenders<T>>().unwrap())
+}
+
+pub struct UserStreamData {
+    pub user: Id<User>,
+    pub room: Id<Room>,
+    pub user_store: DataStore<User>,
+    pub room_store: DataStore<Room>,
+}
+
+impl UserStreamData {
+    fn new(
+        user: Id<User>,
+        room: Id<Room>,
+        user_store: DataStore<User>,
+        room_store: DataStore<Room>,
+    ) -> Self {
+        Self {
+            user,
+            room,
+            user_store,
+            room_store,
+        }
+    }
+}
+
+pub struct UserStreamSubscriber<
+    'a,
+    T: Send + Sync + Clone + 'static,
+    F: FnOnce(&UserStreamData),
+> {
+    id: usize,
+    receiver: UnboundedReceiver<T>,
+    on_drop: Option<Box<F>>,
+    data: UserStreamData,
+    ctl: &'a StreamController,
+}
+
+impl<'a, T: Send + Sync + Clone + 'static, F: FnOnce(&UserStreamData)>
+    UserStreamSubscriber<'a, T, F>
+{
+    pub fn new(
+        id: usize,
+        receiver: UnboundedReceiver<T>,
+        user: &Id<User>,
+        room: &Id<Room>,
+        user_store: DataStore<User>,
+        room_store: DataStore<Room>,
+        on_drop: F,
+        ctl: &'a StreamController,
+    ) -> Self {
+        let on_drop = Some(Box::new(on_drop));
+        let data = UserStreamData::new(user.clone(), room.clone(), user_store, room_store);
+        Self {
+            id,
+            receiver,
+            on_drop,
+            data,
+            ctl,
+        }
+    }
+}
+
+impl<'a, T: Send + Sync + Clone + 'static, F: FnOnce(&UserStreamData)> Stream
+    for UserStreamSubscriber<'a, T, F>
+{
+    type Item = T;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.receiver.poll_next_unpin(cx)
+    }
+}
+
+impl<'a, T: Send + Sync + Clone + 'static, F: FnOnce(&UserStreamData)> Drop
+    for UserStreamSubscriber<'a, T, F>
+{
+    fn drop(&mut self) {
+        if let Some(on_drop) = self.on_drop.take() {
+            on_drop(&self.data);
+        }
+        with_senders::<T, _, _>(&self.ctl, |senders| senders.0.remove(self.id));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        model::{room::RoomChatMsg, user::User},
+        store::DataStore,
+        types::time::Time,
+    };
+
+    #[tokio::test]
+    async fn stream_subscribe_and_publish() {
+        let user_store = DataStore::<User>::new();
+        let room_store = DataStore::<Room>::new();
+        let ctl = StreamController::new();
+
+        let user = User::new("jonah".to_string());
+        let user_id = user.id.clone();
+        let mut room = Room::new();
+        let room_id = room.id.clone();
+        room.init_owner(&user, None).unwrap();
+        room_store.insert(room);
+        user_store.insert(user);
+
+        let sub_ctl = ctl.clone();
+        let sub_user_id = user_id.clone();
+        let sub_room_id = room_id.clone();
+        let sub_task = tokio::spawn(async move {
+            //println!("subscribing...");
+            let mut stream = sub_ctl.subscribe::<RoomChatMsg, _>(
+                &sub_user_id,
+                &sub_room_id,
+                user_store.clone(),
+                room_store.clone(),
+                move |data| {
+                    let room = &mut data.room_store.get(&data.room).unwrap().unwrap();
+                    let user = &data.user_store.get(&data.user).unwrap().unwrap();
+                    room.leave(&user).unwrap();
+                    //println!("dropped");
+                    assert!(!room.is_member(&user.id))
+                },
+            );
+            //println!("subscribed");
+
+            while let Some(msg) = &stream.next().await {
+                //println!("{:?}", msg);
+                assert_eq!(msg.msg, "hello".to_string());
+                assert_eq!(&msg.room, &sub_room_id);
+                assert_eq!(&msg.author, &sub_user_id);
+                break; // abort after 1 message
+            }
+            //println!("done");
+        });
+
+        let pub_task = tokio::spawn(async move {
+            ctl.publish(RoomChatMsg::new(
+                Id::new(),
+                room_id.clone(),
+                user_id.clone(),
+                "hello".to_string(),
+                Time::now(),
+            ));
+            //println!("published!");
+        });
+
+        pub_task.await.unwrap();
+        sub_task.await.unwrap();
     }
 }
---
+++ api/src/types/id.rs
@@ -0,0 +1,158 @@
+use crate::model::Model;
+use std::{hash::Hash, fmt::Display};
+
+use async_graphql::{InputValueError, Scalar, ScalarType, Value};
+use serde::{Deserialize, Serialize};
+
+pub trait IdType:
+    Send + Sync + ToString + PartialEq + Eq + Hash + Sized + Clone + std::fmt::Debug
+{
+    type Error: std::error::Error;
+    fn new() -> Self;
+    fn from_str(id: impl ToString) -> Result<Self, Self::Error>;
+}
+
+#[derive(Debug, Clone, Eq, Hash)]
+pub struct Id<M: Model>(pub <M as Model>::Id);
+
+impl<M: Model> Id<M> {
+    pub fn new() -> Self {
+        Self(<M as Model>::Id::new())
+    }
+    pub fn from_str(id: impl ToString) -> Result<Self, <<M as Model>::Id as IdType>::Error> {
+        Ok(Self(<M as Model>::Id::from_str(id)?))
+    }
+}
+
+impl<M: Model> PartialEq for Id<M> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+#[Scalar]
+impl<M: Model> ScalarType for Id<M> {
+    fn parse(value: async_graphql::Value) -> async_graphql::InputValueResult<Self> {
+        if let Value::String(value_str) = &value {
+            if let Ok(id) = Self::from_str(value_str) {
+                Ok(id)
+            } else {
+                Err(InputValueError::expected_type(value))
+            }
+        } else {
+            Err(InputValueError::expected_type(value))
+        }
+    }
+
+    fn to_value(&self) -> async_graphql::Value {
+        Value::String(self.0.to_string())
+    }
+}
+
+impl<M: Model> Serialize for Id<M> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.0.to_string())
+    }
+}
+
+struct IdVisitor<M: Model>(std::marker::PhantomData<M>);
+
+impl<'de, M: Model> serde::de::Visitor<'de> for IdVisitor<M> {
+    type Value = Id<M>;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a string")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Id::from_str(value).map_err(|_| E::custom("invalid id"))?)
+    }
+}
+
+impl<'de, M: Model> Deserialize<'de> for Id<M> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_string(IdVisitor(std::marker::PhantomData))
+    }
+}
+
+pub trait ToId<M: Model> {
+    fn to_id(&self) -> Result<<M as Model>::Id, <<M as Model>::Id as IdType>::Error>;
+}
+
+impl<M: Model> ToId<M> for Id<M> {
+    fn to_id(&self) -> Result<<M as Model>::Id, <<M as Model>::Id as IdType>::Error> {
+        Ok(self.0.clone())
+    }
+}
+
+impl<M: Model> ToId<M> for &Id<M> {
+    fn to_id(&self) -> Result<<M as Model>::Id, <<M as Model>::Id as IdType>::Error> {
+        Ok(self.0.clone())
+    }
+}
+
+impl<M: Model> ToId<M> for String {
+    fn to_id(&self) -> Result<<M as Model>::Id, <<M as Model>::Id as IdType>::Error> {
+        <M as Model>::Id::from_str(&self)
+    }
+}
+
+impl<M: Model> ToId<M> for &str {
+    fn to_id(&self) -> Result<<M as Model>::Id, <<M as Model>::Id as IdType>::Error> {
+        <M as Model>::Id::from_str(&self)
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
+pub struct UuidId(pub String);
+
+impl Display for UuidId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl IdType for UuidId {
+    type Error = std::convert::Infallible;
+    fn new() -> Self {
+        let uuid_str = uuid::Uuid::new_v4().to_string().replace("-", "")[0..6].to_string();
+        Self(uuid_str)
+    }
+    fn from_str(id: impl ToString) -> Result<Self, Self::Error> {
+        Ok(Self(id.to_string()))
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
+pub struct NumId(pub u32);
+
+impl NumId {
+    pub fn from_u32(id: u32) -> Self {
+        Self(id)
+    }
+}
+
+impl Display for NumId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl IdType for NumId {
+    type Error = std::num::ParseIntError;
+    fn new() -> Self {
+        Self(0)
+    }
+    fn from_str(id: impl ToString) -> Result<Self, Self::Error> {
+        Ok(Self(id.to_string().parse()?))
+    }
+}
