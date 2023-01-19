872ded191e6fb79e745777e678d92abdcfe7d06e
Alexander Maxwell Adewole <alex@bizar.re>
2023-01-19 12:42:49 -0800

    Remove unused scalar impl

--- api/src/types/mod.rs
+++ api/src/types/mod.rs
@@ -1,22 +1,3 @@
 pub mod color;
 pub mod time;
 pub mod token;
-
-use async_graphql::*;
-use musty::prelude::*;
-
-struct IdScalar<M: Model>(Id<M>);
-
-#[Scalar]
-impl<M: Model> ScalarType for IdScalar<M>{
-    fn parse(value: async_graphql::Value) -> async_graphql::InputValueResult<Self> {
-        let json = value.into_json().unwrap();
-        let id = serde_json::from_value(json).unwrap();
-        Ok(IdScalar(id))
-    }
-
-    fn to_value(&self) -> async_graphql::Value {
-        let json = serde_json::to_value(&self.0).unwrap();
-        Value::from_json(json).unwrap()
-    }
-}
